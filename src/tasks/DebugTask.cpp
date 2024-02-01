7f7d658be96673a4ae4b2331c1df853e30419dd7
Edward Browncross <edward.browncross@gmail.com>
2024-02-01 10:45:45 +0000

    print free memory from debug task

--- src/tasks/DebugTask.cpp
+++ src/tasks/DebugTask.cpp
@@ -13,7 +13,7 @@
 class DebugTask : public Task, public TSEvents::EventHandler {
  public:
   DebugTask(Scheduler& s, TSEvents::EventBus& e)
-      : Task(0, TASK_ONCE, &s, false),
+      : Task(10*TASK_SECOND, TASK_FOREVER, &s, false),
         TSEvents::EventHandler(&s, &e) {
     // M5Dial.Display.println("Debug task initialized");
     Serial.println("Debug task initialized");
@@ -22,11 +22,19 @@ class DebugTask : public Task, public TSEvents::EventHandler {
   bool OnEnable() {
     // M5Dial.Display.println("Debug task running");
     Serial.println("Debug task running");
-    return false;
+    return true;
   }
 
   bool Callback() {
-    return false;
+    multi_heap_info_t info;
+    heap_caps_get_info(&info, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT); // internal RAM, memory capable to store data or to create new task
+    Serial.print("Free heap: ");
+    Serial.println(info.total_free_bytes);
+    Serial.print("Minimum free heap: ");
+    Serial.println(info.minimum_free_bytes);
+    Serial.print("Largest free block: ");
+    Serial.println(info.largest_free_block);
+    return true;
   }
 
   void HandleEvent(TSEvents::Event event) {
